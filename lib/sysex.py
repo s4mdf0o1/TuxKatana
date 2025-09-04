2577c63bb950e19398ce64e78e76cecdba262c90
Damien ULRICH <s4mdf0o1@gmail.com>
2025-09-04 10:06:43 +0200

    add sysex

---
+++ lib/sysex.py
@@ -0,0 +1,77 @@
+from ruamel.yaml import YAML
+#from ruamel.yaml.scalarstring import SingleQuotedScalarString as sq
+yaml = YAML(typ="rt")
+from collections import UserDict
+
+DEBUG = True
+def debug( txt ):
+    if DEBUG:
+        print(txt)
+
+
+class SysExML(UserDict):
+    def __init__(self, filepath):
+        super().__init__()
+        self.filepath = filepath
+        with open(filepath, 'r') as f:
+            raw = yaml.load(f)
+        self.data = raw
+
+    def __getitem__(self, key):
+        for k in self.data.keys():
+            if key in self.data[k].keys():
+                return self.from_str(self.data[k][key])
+
+    def from_str(self, s: str):
+        return [int(x, 16) for x in s.split()]
+
+    def to_str( self, d):
+        if isinstance(d, list):
+            return " ".join(f"{i:02x}" for i in d)
+        raise TypeError(f"Argument must be a list, got {type(d).__name__}")
+
+    def save(self):
+        debug(f"save: {self.data}")
+        with open(self.filepath, 'w') as f:
+            yaml.dump(self.data, f)
+
+class SysExMessage:
+    def __init__( self ):
+        debug("SysExMessage.__init__")
+        self.addrs = SysExML("params/sysex.yaml")
+        self.header = [int(v, 16) for v in "41 00 00 00 00 33".split(' ')]
+
+    def get( self, cmd, name, value ):
+        command = self.addrs[cmd]
+        addr = self.addrs[name]
+        data = addr + value
+        cks = [self.checksum(data)]
+        return self.header + command + data + cks
+
+    def get_addr_data( self, msg ):
+        for t in ["F0 ", self.addrs.to_str(self.header)+" ", " F7"]:
+            msg = msg.replace( t, "" )
+        mlst = self.addrs.from_str(msg)
+        cmd = mlst.pop(0)
+        cks = mlst.pop(-1)
+        if cks != self.checksum(mlst):
+            raise ValueError("Checksum Error in {msg} ")
+        return mlst[:4], mlst[4:]
+
+    def decode(self, msg):
+        debug(f"SysExMessage.decode({msg.hex()})")
+        addr, data =  self.get_addr_data(msg.hex())
+        debug(f"{self.addrs.to_str(addr)}: {self.addrs.to_str(data)}")
+
+    def decode_ident( self, data ):
+        debug(f"decode_ident({[hex(i) for i in data]})")
+        #print(app.katana)
+
+    def checksum( self, data ):
+        return (128 - (sum(data) % 128)) % 128
+    
+    def create( self, cmd, addr, val ):
+        data=addr + val
+        return mido.Message('sysex', self.header + cmd + data + self.checksum(data))
+
+
